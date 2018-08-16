fa896d3f53f37bcfd3a1a7bb839ec5d687a6a5de
LasseNatvig <Lasse.Natvig@ntnu.no>
2018-08-16 20:32:23 +0200

    added ex1

---
+++ lec1/Ex1/Ex1/Lec1Ex1.cpp
@@ -0,0 +1,9 @@
+// Lec1Ex1.cpp
+#include "std_lib_facilities.h"
+int main()
+{
+	cout << "The rather old \"Hello, World!\" message\n"; 
+	cout << "Type RETURN to exit";
+	keep_window_open();
+	return 0;
+}
