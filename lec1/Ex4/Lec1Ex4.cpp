157f5e1b12d27af8522c2e1388ecee0344a47953
LasseNatvig <Lasse.Natvig@ntnu.no>
2018-08-17 20:31:59 +0200

    ex 4 1st version

--- lec1/Ex4/Lec1Ex4.cpp
+++ lec1/Ex4/Lec1Ex4.cpp
@@ -1,11 +1,12 @@
 // Lec1Ex4.cpp
-#include <iostream>
-// string
-
-// vector
+#include "std_lib_facilities.h"
 int main() {
-	char c;
-	std::cin >> c;
-	return 0;
+	vector<int> numbers{ -3, 3, 5, 8888 };
 
+	cout << "The numbers are:\n";
+	for (int i = 0; i < numbers.size(); i++) {
+		cout << numbers[i] << endl;
+	}
+
+	keep_window_open();
 }
