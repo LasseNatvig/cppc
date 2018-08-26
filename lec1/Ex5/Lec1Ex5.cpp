ab3d3152b050c5ef383344aaebf74eff399e459f
LasseNatvig <Lasse.Natvig@ntnu.no>
2018-08-26 23:22:05 +0200

    1st version of lec1ex4 string

--- lec1/Ex5/Lec1Ex5.cpp
+++ lec1/Ex5/Lec1Ex5.cpp
@@ -1,12 +1,20 @@
 // Lec1Ex5.cpp      Demonstrates simple use of string
 #include "std_lib_facilities.h"
 int main() {
-	vector<int> numbers{ -3, 3, 5, 8888 };
+	string s1("Hello");
+	string s2 = "World";
+	string s3 = s1 + " " + s2 + "!";
+	cout << "1) " << s3 << endl;
 
-	cout << "The numbers *****************' CHANGE ALL CODE HERE ****** are:\n";
-	for (unsigned int i = 0; i < numbers.size(); i++) {
-		cout << numbers[i] << endl;
-	}
+	string s4 = "Strange ";
+	s3.insert(6, s4);
+	cout << "2) " << s3 << "\n";
+
+	cout << "3) Length of s3 is: " << s3.size() << '\n';
+
+	// access to char
+
+	// substr ...
 
 	keep_window_open();
 }
