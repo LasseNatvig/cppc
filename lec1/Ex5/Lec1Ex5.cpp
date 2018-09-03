49c42efb8926b495c42a92a451623d31623b128d
LasseNatvig <Lasse.Natvig@ntnu.no>
2018-09-03 11:24:04 +0200

    first version of Lec1Ex5 done, remember its TODOs. Lec2Ex1 only very initial code

--- lec1/Ex5/Lec1Ex5.cpp
+++ lec1/Ex5/Lec1Ex5.cpp
@@ -1,20 +1,24 @@
-// Lec1Ex5.cpp      Demonstrates simple use of string
+// Lec1Ex5.cpp      Demonstrates simple use of string, and more
 #include "std_lib_facilities.h"
 int main() {
-	string s1("Hello");
-	string s2 = "World";
-	string s3 = s1 + " " + s2 + "!";
+	cout << "Enter your name (followed by return/enter)\n";
+	string name = ""; // string object name initialized to empty string // TODO needed?, is there default initializer?
+	cin >> name; // read a string from cin (= console in, keyboard)
+	string s1 = "Hello"; // sets s1 to Hello
+	string s2(",have a nice day!"); // sets s2 to a longer text-string, containing blanks. Also alternative syntax () instead of =
+	string s3 = s1 + " " + name + s2; // + is defined as a concatenate operator for string
 	cout << "1) " << s3 << endl;
 
-	string s4 = "Strange ";
-	s3.insert(6, s4);
+	// We want to insert a blank after the comma, what is its position?
+	int pos = s1.length() + 1 + name.length() + 1;
+	s3.insert(pos, " ");
 	cout << "2) " << s3 << "\n";
 
-	cout << "3) Length of s3 is: " << s3.size() << '\n';
-
-	// access to char
-
-	// substr ...
+	/*** TODO; supplement with what is needed for exercise 1 and 2
+	- access to char ?
+	- substr ... TODO Not listed in PPP at page 851?
+	*/
 
 	keep_window_open();
+	keep_window_open(); // TODO far from elegant that I must have two of these !  // but easily solved by debugger and breakpoint
 }
