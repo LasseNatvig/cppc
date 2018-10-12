d7cd78e0f0fc159574d532e1b5cce60cf13b86cf
LasseNatvig <Lasse.Natvig@ntnu.no>
2018-10-12 16:43:04 +0200

    extended with to_string

--- lec1/Ex5/Lec1Ex5.cpp
+++ lec1/Ex5/Lec1Ex5.cpp
@@ -5,7 +5,7 @@ int main() {
     string name = ""; // string object name initialized to empty string // TODO needed?, is there default initializer?
 	cin >> name; // read a string from cin (= console in, keyboard)
 	string s1 = "Hello"; // sets s1 to Hello
-	string s2(", have a nice day!"); // sets s2 to a longer text-string, containing blanks. Also alternative syntax () instead of =
+	string s2(",have a nice day!"); // sets s2 to a longer text-string, containing blanks. Also alternative syntax () instead of =
 	string s3 = s1 + " " + name + s2; // + is defined as a concatenate operator for string
 	cout << "1) " << s3 << endl;
 
@@ -14,11 +14,9 @@ int main() {
 	s3.insert(pos, " ");
 	cout << "2) " << s3 << "\n";
 
-	/*** TODO; supplement with what is needed for exercise 1 and 2
-	- access to char ?
-	- substr ... TODO Not listed in PPP at page 851?
-	*/
+	cout << "The length of string s3 as int: " << s3.length() <<
+		" and as string: " << to_string(s3.length());
 
-	keep_window_open();
-	keep_window_open(); // TODO far from elegant that I must have two of these !  // but easily solved by debugger and breakpoint
+	cin.ignore();
+	keep_window_open();  
 }
