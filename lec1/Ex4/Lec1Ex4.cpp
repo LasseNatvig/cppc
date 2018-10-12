ad4fc1f24a930aefd14f75c323bfed60b84bf2ad
LasseNatvig <Lasse.Natvig@ntnu.no>
2018-10-12 12:33:22 +0200

    extended vector example

--- lec1/Ex4/Lec1Ex4.cpp
+++ lec1/Ex4/Lec1Ex4.cpp
@@ -4,14 +4,23 @@
 - how to append new elements
 - size of vector
 - how to update an element in a vector
+- function treturning a vector
 */
 #include "std_lib_facilities.h"
+
+vector<double> roots() {
+	vector <double> v;
+	for (int i = 2; i < 11; i++) {
+		v.push_back(sqrt(i));
+	}
+	return v;
+}
+
 int main() {
 	vector<int> numbers{ -3, 3, 5, 8888 };
-
 	vector<int> v{1, 4, 2, 3, 5};
 
-	cout << "The numbers are:\n"; // unsigned needed to avoid warning in MS-VS
+	cout << "The numbers are:\n"; // unsigned int needed to avoid warning in MS-VS
 	for (unsigned int i = 0; i < numbers.size(); i++) {
 		cout << numbers[i] << " ";
 	}
@@ -27,6 +36,7 @@ int main() {
 	for (auto e : numbers) {
 		cout << e << " ";
 	}
+
 	vector<double> realNumbers{}; // initially empty
 	realNumbers.push_back(3.0);
 	realNumbers.push_back(pow(3.0, 2.0));
@@ -34,6 +44,10 @@ int main() {
 	cout << "\n... vector of doubles, power and square root functions:\n";
 	for (auto e : realNumbers) {
 		cout << e << " ";
+	}
+	cout << endl << endl;
+	for (auto r : roots()) {
+		cout << r << " ";
 	}
 
 	keep_window_open();
