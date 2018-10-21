6225cfce42f32a2d72f27de246dc7c8821311100
LasseNatvig <Lasse.Natvig@ntnu.no>
2018-10-21 20:45:50 +0200

    Example Lec2Ex2 first version

--- lec2/Ex2/Lec2Ex2.cpp
+++ lec2/Ex2/Lec2Ex2.cpp
@@ -1,9 +1,25 @@
-// Lec2Ex2.cpp      Demonstrates ....
+// Lec2Ex2.cpp, demonstrates two functions with a bug, both can 
+// "fall through the end of the function" and that can give undefined results
 #include "../../std_lib_facilities.h"
+int maxOfTwo(int a, int b) {
+	if (a > b)
+		return a;
+	else if (b > a)
+		return b;
+}
+double maxOfTwo(double a, double b) {
+	if (a > b)
+		return a;
+	else if (b > a)
+		return b;
+}
 int main() {
-	cout << "Enter your";
-	//vanlig
+	cout << maxOfTwo(2.0, 1.99999999) << endl;
+	cout << maxOfTwo(2.0, 3.0) << endl;
+	cout << maxOfTwo(2.0, 2.0) << endl;
+	cout << maxOfTwo(2.0, 1.9999999999999999) << endl;
 
-	//	funksjon retur  etc
-
+	cout << maxOfTwo(4, 5) << endl;
+	cout << maxOfTwo(4, 4) << endl; 
+	keep_window_open();
 }
