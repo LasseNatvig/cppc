4e5ed67027c81b762ac125f296294cea88c658d2
LasseNatvig <Lasse.Natvig@ntnu.no>
2018-10-21 21:37:14 +0200

    started lec2 ex 3

--- lec2/Ex3/Lec2Ex3.cpp
+++ lec2/Ex3/Lec2Ex3.cpp
@@ -1,6 +1,43 @@
 // Lec2Ex3.cpp      Demonstrates ....
 #include "../../std_lib_facilities.h"
+
+const int maxNum = 5000000;
+
+vector<int> generateVector(unsigned int n) {
+	vector<int> temp;
+	for (unsigned int i = 0; i < n; i++) {
+		temp.push_back(randint(maxNum));
+	}
+	return temp;
+}
+
+void searchVectorCopy(vector<int> v, int num) { // copy by value
+	for (unsigned int i = 0; i < v.size(); ++i) {
+		if (v[i] == num)
+			cout << "Num: " << num << "found at index " << i << endl;
+	}
+	cout << endl;
+}
+
+void searchVectorRef(vector<int> v_r, int num) { // copy by value
+	for (unsigned int i = 0; i < v_r.size(); ++i) {
+		if (v_r[i] == num)
+			cout << "Num: " << num << "found at index " << i << endl;
+	}
+	cout << endl;
+}
+
+void searchVectorConstRef(const vector<int>& v_cr, int num) { // copy by value
+	for (unsigned int i = 0; i < v_cr.size(); ++i) {
+		if (v_cr[i] == num)
+			cout << "Num: " << num << "found at index " << i << endl;
+	}
+	cout << endl;
+}
+
 int main() {
-	cout << "Enter your";
-	//v ytelse const ref
+	vector<int> V = generateVector(10000000);
+	searchVectorCopy(V, 9999);	
+	searchVectorRef(V, 9999);	
+	searchVectorConstRef(V, 9999);
 }
