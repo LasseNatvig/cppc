eacacb2135ed98b1c8f2be8e8e9ed4703b68244b
LasseNatvig <Lasse.Natvig@ntnu.no>
2018-11-11 14:19:50 +0100

    adjusted and extendes string example

--- lec2/Ex1/Lec2Ex1.cpp
+++ lec2/Ex1/Lec2Ex1.cpp
@@ -4,7 +4,8 @@ int main() {
 	cout << "Enter your name (followed by return/enter)\n";
 	string name = ""; // string object name initialized to empty string // TODO needed?, is there default initializer?
 	cin >> name; // read a string from cin (= console in, keyboard)
-	string s1 = "Hello"; // sets s1 to Hello
+	string s1 = "Hello"; // sets s1 to Hello, very common syntax
+	string s1a = { "Hello2" }; // alternative, recommended syntax
 	string s2(",have a nice day!"); // sets s2 to a longer text-string, containing blanks. Also alternative syntax () instead of =
 	string s3 = s1 + " " + name + s2; // + is defined as a concatenate operator for string
 	cout << "1) " << s3 << endl;
@@ -17,7 +18,10 @@ int main() {
 	cout << "The length of string s3 as int: " << s3.length() <<
 		" and as string: " << to_string(s3.length()) << endl;
 
-	cin.ignore(); // without this we need two call to keep_window_open!
-	keep_window_open();	
+	double d = stod("17.18");
+	int i = stoi("190");
+	long l = stol("17184567999");
+	cout << d << " // " << i << " // " << l << endl;
+
 	keep_window_open();
 }
