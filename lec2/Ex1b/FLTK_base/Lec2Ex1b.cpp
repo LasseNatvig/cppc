d95633312c7fff010c9b88bdf0c03f83db2cbc25
LasseNatvig <Lasse.Natvig@ntnu.no>
2018-11-18 00:04:23 +0100

    example numbering and new slides

--- lec2/Ex1b/FLTK_base/Lec2Ex1b.cpp
+++ lec2/Ex1b/FLTK_base/Lec2Ex1b.cpp
@@ -23,7 +23,7 @@ int min(vector<int> v) {
 int main() {
 	Point tl{ 100, 100 }; // tl is Top-Left corner of our window
 	Point origo{ 40, 255 };
-	Simple_window win(tl, 400, 300, "Lecture 2 Example 5b");
+	Simple_window win(tl, 400, 300, "Lecture 2 Example 1b");
 	Axis xa(Axis::x, origo, 330, 11, "Month");
 	win.attach(xa); // attach xa to the window, win
 	xa.set_color(Color::black);
