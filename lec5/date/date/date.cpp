011985050832cec6aa036e057d7375bd7dd999a2
LasseNatvig <Lasse.Natvig@ntnu.no>
2019-01-28 15:52:32 +0100

    polish

--- lec5/date/date/date.cpp
+++ lec5/date/date/date.cpp
@@ -13,7 +13,6 @@ Date::Date(int y, int m, int d) { // check for valid date and initialize
 	cout << "checking valid date NOT completly implemented\n";
 	// But see exam May 2018 problem 2a..2e 
 	if (d > 31) {
-		//throw "Invalid date passed to constructor"; // works but not recommended
 		throw exception("Invalid date passed to constructor"); // gives error message
 	}
 	else {
@@ -22,12 +21,13 @@ Date::Date(int y, int m, int d) { // check for valid date and initialize
 		day = d;
 	}
 }
+
 void Date::add_day(int n) {	// increase the Date by n days
 	cout << "add_day NOT implemented\n"; // not even in the PPP-book
 }
 void print_day(const Date& day) {
 	vector<string> months{ "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec" };
-	cout << "Date: " << day.day << " " << months[day.month-1] << " " << day.year << endl;
+	cout << "Date: " << day.day << " " << months[day.month - 1] << " " << day.year << endl;
 }
 
 class Date_s { // short for Date_safe
@@ -45,7 +45,7 @@ public:
 Date_s::Date_s(int y, int m, int d) : year{ y }, month{ m }, day{ d } {
 	// above line uses so called (member) initializer list (PPP 9.4.4.)
 	// This constructor should check that (y, m, d) is a valid date
-	cout << "checking valid date NOT completely implemented\n"; 
+	cout << "checking valid date NOT completely implemented\n";
 	if (d > 31) {
 		throw exception("Invalid date passed to constructor");
 	}
@@ -62,8 +62,8 @@ int main() try {
 	// Date my_birthday; // error: no default contructor
 	// Date today{ 12, 24, 2007 };  // gives run time error (exception)
 	Date last{ 2000, 12, 31 }; // OK, recommended style
-	Date next = { 2014, 2, 14 }; // OK, slightly verbose
-	Date christmas = Date{ 1976, 12, 24 }; // OK, verbose
+	Date next = { 2014, 2, 14 }; // slightly verbose, acceptable but not so good
+	Date christmas = Date{ 1976, 12, 24 }; // verbose, acceptable but not so good
 
 	Date whatAday(1958, 5, 20); // OK, but now a rather OLD style (C++98) (PPP p. 312)
 
@@ -73,15 +73,17 @@ int main() try {
 	//************************
 	// (2)
 	print_day(christmas);
-	christmas.month = 14; // free access to member variable in struct date makes it easy to produce an illegal date
-	// imagine much later, in another part of a huge program, after hours of execution you will print it ("Time bomb")
-	print_day(christmas);  // run-time error
+	christmas.month = 14; // Free access to member variable in struct date makes it easy to produce an illegal date.
+	// Imagine much later, in another part of a huge program, after hours of execution you will print it ... ("Time bomb")
+	//print_day(christmas);  // run-time error
 
 	//************************
 	// (3)
-	Date_s fineDay{ 2019, 1, 14 };
+	Date_s fineDay{ 2019, 1, 29 };
 	// cout << "fineDay.month = " << fineDay.month << endl;  // error, cannot acces private member
 	cout << "fineDay.month = " << fineDay.get_month() << endl;
+	Date_s someDay{ 2019, 1, 299 };  // demonstrates exception thrown by Date_s constructor
+
 	return 0;
 }
 catch (exception& e) {
