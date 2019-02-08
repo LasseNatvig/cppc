1ab6f240856f57ac988914095c80a0ba1e418f17
LasseNatvig <Lasse.Natvig@ntnu.no>
2019-02-08 12:20:18 +0100

    testing

--- lec8/map_sort/map_sort/map_sort.cpp
+++ lec8/map_sort/map_sort/map_sort.cpp
@@ -1,3 +1,4 @@
+// map_sort.cpp
 #include "std_lib_facilities.h"
 
 struct Student {
@@ -5,11 +6,9 @@ struct Student {
 	string lastName;
 	int id;
 };
-
 ostream& operator<<(ostream& os, const Student& s) {
 	return os << s.id << ":" << s.firstName << " " << s.lastName;
 }
-
 //bool operator<(const Student& left, const Student& right) {
 //	return left.firstName < right.firstName;
 //}
@@ -18,15 +17,19 @@ bool operator<(const Student& left, const Student& right) {
 	return left.id < right.id;
 }
 
-int main(){
+int main() {
 	map<Student, double> gradeStats;
-	Student s1{"Ola", "Helt", 1 };
-	Student s2{"Kari", "Heltinne", 2};
-	Student s3{"Bjarne", "Helt", 007};
+	Student s1{ "Ola", "Helt", 1 };
+	Student s2{ "Kari", "Heltinne", 2 };
+	Student s3{ "Bjarne", "Helt", 007 };
 
 	gradeStats[s1] = 2.0;
 	gradeStats[s2] = 1.8;
 	gradeStats[s3] = 1.99;
 
-	for (const auto result : gradeStats) cout << result.first << " " << result.second << endl;
+	for (const auto result : gradeStats) {
+		cout << result.first << " "
+			<< result.second << endl;
+	}
 }
+
