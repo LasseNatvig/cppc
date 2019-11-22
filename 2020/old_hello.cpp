a2d2aaeeafb1bed1c942b5905cb70748c721a653
LasseNatvig <Lasse.Natvig@ntnu.no>
2019-11-22 21:17:40 +0100

    Delete old_hello.cpp

--- 2020/old_hello.cpp
+++
@@ -1,7 +0,0 @@
-// old_hello.cpp
-#include "std_lib_facilities.h" 
-int main() {
-	cout << "The rather old \"Hello, World!\" message\n";
-	// keep_window_open(); // Not needed when using project template TDT4102_std_lib_facilities 
-	return 0;
-}
